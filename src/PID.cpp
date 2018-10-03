6cd4494efb93d009651f4f1499f48621cfe87ca7
wuzhanglin <jason.lin.wu@163.com>
2018-10-03 12:58:53 +0800

    Fix coding style issues
    Fix some coding style issue like text-indent, and etc. These are safe changes.

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,21 +1,24 @@
 #include "PID.h"
 
-using namespace std;
-
-/*
-* TODO: Complete the PID class.
-*/
-
-PID::PID() {}
-
-PID::~PID() {}
-
-void PID::Init(double Kp, double Ki, double Kd) {
+PID::PID()
+{
+    
 }
 
-void PID::UpdateError(double cte) {
+PID::~PID()
+{
+    
 }
 
-double PID::TotalError() {
+void PID::Init(double Kp, double Ki, double Kd)
+{
+}
+
+void PID::UpdateError(double cte)
+{
+}
+
+double PID::TotalError()
+{
 }
 
